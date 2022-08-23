ea2fd2829f2df445964911613b4816acd3524800
ythuang13 <ythuang13@gmail.com>
2022-08-23 16:53:43 -0700

    Implement basic mindcontrolstone

--- Assets/Scripts/StoneMechanics/StoneTypes/MindControlStone.cs
+++ Assets/Scripts/StoneMechanics/StoneTypes/MindControlStone.cs
@@ -4,6 +4,11 @@ namespace StoneTypes
 {
     public class MindControlStone : StoneBehaviour
     {
+        private Vector2 mousePosition;
+        
+        [SerializeField]
+        private float controlDelay = 0.5f;
+
         public MindControlStone(Rigidbody2D stoneBody) : base(stoneBody)
         {
             stoneBody.gameObject.tag = StoneTags.MindControl;
@@ -24,11 +29,15 @@ namespace StoneTypes
         public override void Update()
         {
             base.Update();
+            this.mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
         public override void FixedUpdate()
         {
             base.FixedUpdate();
+
+            // move towards mouse
+            this._stoneBody.position = Vector2.Lerp(this._stoneBody.position, this.mousePosition, Time.fixedDeltaTime * 5);
         }
 
         public override void Destroy()
