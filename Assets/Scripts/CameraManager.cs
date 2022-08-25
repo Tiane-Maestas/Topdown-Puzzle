070d3de563045cef78624d9ac9709d9eebb55031
Tiane Maestas <tiane.developer@gmail.com>
2022-08-25 15:10:25 -0700

    Added a simple camera script

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 nextCameraPosition;
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        nextCameraPosition = new Vector3(player.position.x, player.position.y, -10);
+    }
+
+    private void FixedUpdate()
+    {
+        nextCameraPosition.x = player.position.x;
+        nextCameraPosition.y = player.position.y;
+        this.transform.position = nextCameraPosition;
+    }
+}
